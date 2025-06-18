cacbb7462092f9b65d1ffa922a238b42f4006a5d
artem smola <artemsmola055@gmail.com>
2025-06-18 19:12:26 +0300

    Fixed formatting

--- src/road_recognition/manager.hpp
+++ src/road_recognition/manager.hpp
@@ -65,7 +65,8 @@ private:
   double EvaluateIoU(const cv::Mat &marking_res, const cv::Mat &ground_truth);
   double EvaluateAccuracy(const cv::Mat &marking_res,
                           const cv::Mat &ground_truth);
-  double EvaluatePrecision(const cv::Mat &marking_res, const cv::Mat &ground_truth);
+  double EvaluatePrecision(const cv::Mat &marking_res,
+                           const cv::Mat &ground_truth);
   void PrintHistogram(const std::vector<double> &data, int num_bins);
 
   std::string path_to_txt_;
@@ -75,6 +76,7 @@ class RoiManager {
 public:
   RoiManager(Reader &reader);
   void Process();
+
 private:
   Reader &reader_;
   FlowHandler handler_;
